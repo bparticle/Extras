04395aeb47d4c336fbbf62b2c4c6c61c396c98e5
Bruno Patyn <bruno@particle-in-a-box.com>
2019-03-18 19:06:24 +0100

    add init.lua

---
+++ init.lua
@@ -0,0 +1,13 @@
+local units = {
+    {category = "Sequencer"},
+    {title="Trig8",moduleName="Trig8",keywords="sequencer, trigger"}
+  }
+
+  return {
+    title = "Extras",
+    name = "Bparticle",
+    contact = "via O|D Forums @bparticle",
+    keyword = "Extras",
+    units = units
+  }
+  
